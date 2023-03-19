ef17c313bf63f9f21679b6e3990c32c81e4cb609
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-19 21:19:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from pyrogram import Client, filters
 from helper_funcs.helpers import humanbytes, convert
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("6207729322:AAGDcDHUrWAIyxZyE3bScXvcYgGctRRmfZ8", "")
 
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("20777636", ""))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("5b184795fd3f77557caf195b9ba9ba8c", "")
 
 
 app = Client("mdisk", bot_token=TG_BOT_TOKEN, api_hash=API_HASH, api_id=APP_ID)
